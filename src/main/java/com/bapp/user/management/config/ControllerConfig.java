4c7b04a97562039bfe4112921097d18bb623ec5a
maiorovi <maiorov.prog@gmail.com>
2019-06-18 16:32:38 +0300

    added integration with configuration server

--- src/main/java/com/bapp/user/management/config/ControllerConfig.java
+++ src/main/java/com/bapp/user/management/config/ControllerConfig.java
@@ -6,8 +6,11 @@ import com.bapp.user.management.repository.InMemoryUserRepository;
 import com.bapp.user.management.repository.UserRepository;
 import com.bapp.user.management.service.user.DefaultUserService;
 import com.bapp.user.management.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -17,10 +20,15 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
+@Slf4j
 public class ControllerConfig {
+
+    @Value("${foo}")
+    private String test;
 
     @Bean
     public UserRepository userRepository() {
+        log.info(">>> foo = " + test);
         return new InMemoryUserRepository();
     }
 
